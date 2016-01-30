32f27b1096c109739fd57cb4d30b87352ffe5ef4
craigthelinguist <aaroncraig@protonmail.ch>
2016-01-30 17:09:48 +1300

    Game of life now checks adjacent diagonals

--- life.c
+++ life.c
@@ -6,13 +6,16 @@
 
 typedef enum { DEAD, ALIVE } Cell;
 
-
 int NeighboursAlive (WORLD *world, uint16_t row, uint16_t col);
 int RightOf (WORLD *world, uint16_t row, uint16_t col);
 int LeftOf (WORLD *world, uint16_t row, uint16_t col);
-int AboveOf (WORLD *world, uint16_t row, uint16_t col);
-int BelowOf (WORLD *world, uint16_t row, uint16_t col);
+int TopOf (WORLD *world, uint16_t row, uint16_t col);
+int BottomOf (WORLD *world, uint16_t row, uint16_t col);
 
+int TopLeftOf (WORLD *world, uint16_t row, uint16_t col);
+int TopRightOf (WORLD *world, uint16_t row, uint16_t col);
+int BottomLeftOf (WORLD *world, uint16_t row, uint16_t col);
+int BottomRightOf (WORLD *world, uint16_t row, uint16_t col);
 
 void
 GameOfLife (WORLD *world)
@@ -31,17 +34,11 @@ GameOfLife (WORLD *world)
 
          // Get info about current cell.
          int neighbours = NeighboursAlive(world, row, col);
-         int cell = World_CellAt(world, row, col);
          int idx = INDEX(row, col);
 
          // Under or over population kills the cell.
-         if (cell && (neighbours == 1 || neighbours == 4))
-            new_board[idx] = DEAD;
-
-         // Three neighbours give life to a dead cell.
-         if (!cell && neighbours == 3)
-            new_board[idx] = ALIVE;
-
+         if (neighbours == 3) new_board[idx] = ALIVE;
+         else new_board[idx] = DEAD;
       }
    }
 
@@ -65,8 +62,12 @@ int NeighboursAlive (WORLD *world, uint16_t row, uint16_t col)
    int count = 0;
    if (RightOf(world, row, col)) count++;
    if (LeftOf(world, row, col)) count++;
-   if (AboveOf(world, row, col)) count++;
-   if (BelowOf(world, row, col)) count++;
+   if (TopOf(world, row, col)) count++;
+   if (BottomOf(world, row, col)) count++;
+   if (TopLeftOf(world, row, col)) count++;
+   if (TopRightOf(world, row, col)) count++;
+   if (BottomLeftOf(world, row, col)) count++;
+   if (BottomRightOf(world, row, col)) count++;
    return count;
 }
 
@@ -86,7 +87,7 @@ int LeftOf (WORLD *world, uint16_t row, uint16_t col)
    return !wraparound ? DEAD : World_CellAt(world, row, World_Width(world) - 1);
 }
 
-int AboveOf (WORLD *world, uint16_t row, uint16_t col)
+int TopOf (WORLD *world, uint16_t row, uint16_t col)
 {
    if (row > 0)
       return World_CellAt(world, row - 1, col);
@@ -94,12 +95,82 @@ int AboveOf (WORLD *world, uint16_t row, uint16_t col)
    return !wraparound ? DEAD : World_CellAt(world, World_Height(world) - 1, col);
 }
 
-int BelowOf (WORLD *world, uint16_t row, uint16_t col)
+int BottomOf (WORLD *world, uint16_t row, uint16_t col)
 {
    if (row < World_Height(world) - 1)
       return World_CellAt(world, row + 1, col);
    char wraparound = World_WrapsHorizontally(world);
    return !wraparound ? DEAD : World_CellAt(world, 0, col);
 }
+
+int TopLeftOf (WORLD *world, uint16_t row, uint16_t col)
+{
+   if (row > 0 && col > 0)
+      return World_CellAt(world, row - 1, col - 1);
+   char wraparound_top = World_WrapsVertically(world);
+   char wraparound_side = World_WrapsHorizontally(world);
+
+   if ((!row && !wraparound_top)
+       || (!col && !wraparound_side))
+      return DEAD;
+
+   int newrow = row == 0 ? World_Height(world) - 1 : row - 1;
+   int newcol = col == 0 ? World_Width(world) - 1 : col - 1;
+   return World_CellAt(world, newrow, newcol);
+
+}
+
+int TopRightOf (WORLD *world, uint16_t row, uint16_t col)
+{
+   if (row > 0 && col < World_Width(world) - 1)
+      return World_CellAt(world, row - 1, col + 1);
+   char wrap_top = World_WrapsVertically(world);
+   char wrap_side = World_WrapsHorizontally(world);
+   
+   if ((!row && !wrap_top)
+       || (col == World_Width(world) - 1 && !wrap_side))
+      return DEAD;
+
+   uint16_t newrow = row == 0 ? World_Height(world) - 1 : row - 1;
+   uint16_t newcol = col == World_Width(world) - 1 ? 0 : col + 1;
+   return World_CellAt(world, newrow, newcol);
+
+}
+
+int BottomLeftOf (WORLD *world, uint16_t row, uint16_t col)
+{
+   if (row < World_Height(world) - 1 && col > 0)
+      return World_CellAt(world, row + 1, col - 1);
+   char wrap_top = World_WrapsVertically(world);
+   char wrap_side = World_WrapsHorizontally(world);
+
+   if ((!col && !wrap_side)
+       || (row == World_Height(world) - 1 && !wrap_top))
+      return DEAD;
+
+   uint16_t newrow = row == World_Height(world) - 1 ? 0 : row + 1;
+   uint16_t newcol = col == 0 ? World_Width(world) - 1 : col - 1;
+   return World_CellAt(world, newrow, newcol);
+}
+
+int BottomRightOf (WORLD *world, uint16_t row, uint16_t col)
+{
+   if (row < World_Height(world) - 1 && col < World_Width(world) - 1)
+      return World_CellAt(world, row + 1, col + 1);
+   char wrap_top = World_WrapsVertically(world);
+   char wrap_side = World_WrapsHorizontally(world);
+
+   if ((col == World_Width(world) - 1 && !wrap_side)
+       || (row == World_Height(world) - 1 && !wrap_top))
+      return DEAD;
+
+   uint16_t newrow = row == World_Height(world) - 1 ? 0 : row + 1;
+   uint16_t newcol = col == World_Width(world) - 1 ? 0 : col + 1;
+   return World_CellAt(world, newrow, newcol);
+
+
+}
+
+
 
 
